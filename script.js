7afbdfc53fcd42daabf9c1a17cf31995d1eee6b4
Matheo Lorenzo <130951732+MatheoLorenzo@users.noreply.github.com>
2024-09-26 01:39:42 -0300

    Finalizei a image-gallery e finalizando a purchase-info

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function changeImage(imageSrc) {
     document.getElementById('productImage').src = imageSrc;
 }
+
+const increaseBtn = document.querySelector('.btn-increase');
+const decreaseBtn = document.querySelector('.btn-decrease');
+const quantityInput = document.getElementById('quantidade');
+
+increaseBtn.addEventListener('click', () => {
+    let currentValue = parseInt(quantityInput.value);
+    quantityInput.value = currentValue + 1;
+});
+
+decreaseBtn.addEventListener('click', () => {
+    let currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+    }
+});
